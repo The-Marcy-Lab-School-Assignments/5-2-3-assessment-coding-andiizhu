e3f7608b52f42c724293101d7ede462385dda36d
andiizhu <azhu0317@gmail.com>
2024-07-18 16:43:56 -0400

    finished Section 3

--- src/2-factory-function.js
+++ src/2-factory-function.js
@@ -1,7 +1,21 @@
-const bankAccountFactory = () => {
-
+const bankAccountFactory = (balance = 0) => {
+	let bankBalance = balance;
+	const bankAccount = {
+		checkBalance() {
+			return bankBalance;
+		},
+		add(num) {
+			bankBalance = bankBalance + num;
+			console.log(`$${num} added.`);
+		},
+		subtract(num) {
+			bankBalance = bankBalance - num;
+			console.log(`$${num} subtracted.`);
+		},
+	};
+	return bankAccount;
 };
 
 module.exports = {
-  bankAccountFactory,
+	bankAccountFactory,
 };
