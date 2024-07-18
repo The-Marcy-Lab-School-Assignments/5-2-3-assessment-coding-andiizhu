b16203a5a734933f833344cad1a553a6d6d888ca
andiizhu <azhu0317@gmail.com>
2024-07-18 15:09:59 -0400

    finished Section 1

--- src/0-has-many-belongs-to.js
+++ src/0-has-many-belongs-to.js
@@ -1,19 +1,52 @@
-const { getId } = require('./utils');
+const { getId } = require("./utils");
 
 class ToDoItem {
-  constructor() {
-    this.id = getId();
-  }
+	constructor(description, priorityLevel, dueDate) {
+		this.description = description;
+		this.priorityLevel = priorityLevel;
+		this.isDone = false;
+		this.dueDate = dueDate;
+		this.id = getId();
+	}
+	printDescription() {
+		return this.description;
+	}
 }
 
-
 class ToDoList {
-  constructor() {
-    this.id = getId();
-  }
+	static #allLists = [];
+	currentList = [];
+	constructor(name) {
+		this.name = name;
+		this.id = getId();
+		ToDoList.#allLists.push(this);
+	}
+	createItem(description, priorityLevel) {
+		let newItem = new ToDoItem(description, priorityLevel);
+		this.currentList.push(newItem);
+		return newItem;
+	}
+	getItems() {
+		return [...this.currentList];
+	}
+	getCompletedCount() {
+		return [...this.currentList].filter((e) => e.isDone === true).length;
+	}
+	getID() {
+		return this.id;
+	}
+	static list() {
+		return [...ToDoList.#allLists];
+	}
+	static findBy(id) {
+		return [...ToDoList.#allLists].filter((e) => e.id === id)[0];
+	}
+	static allListLength() {
+		return [...ToDoList.#allLists].length;
+	}
 }
 
 module.exports = {
-  ToDoItem,
-  ToDoList
+	ToDoItem,
+	ToDoList,
 };
